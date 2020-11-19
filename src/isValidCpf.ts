05a6ca9089e2941e20fb18c4483fff4d3b024cbb
Michael Amaral <michael.amaral@platformbuilders.io>
2020-11-19 04:12:51 -0300

    fixing cpf and cnpj validations

--- src/isValidCpf.ts
+++ src/isValidCpf.ts
@@ -1,5 +1,5 @@
 import * as CPF from '@fnando/cpf';
 import { toOnlyNumbers } from '@platformbuilders/helpers';
 
-export const isValidCpf = (value: string): boolean =>
-  CPF.isValid(toOnlyNumbers(value));
+export const isValidCpf = (value?: string): boolean =>
+  value ? CPF.isValid(toOnlyNumbers(value)) : false;
