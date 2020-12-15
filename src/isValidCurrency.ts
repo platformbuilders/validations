38e0f57f36d539a4215d47ab7292d412b6c13b9b
Michael Amaral <michael.amaral@platformbuilders.io>
2020-12-15 14:36:53 -0300

    updating taxonomy

--- src/isValidCurrency.ts
+++ src/isValidCurrency.ts
@@ -3,5 +3,5 @@ import { currencyToNumber } from '@platformbuilders/helpers';
 export const isValidCurrency = (currency: string): boolean =>
   !!currency && /^R\$[1-9]\d{0,2}(\.\d{3})*,\d{2}$/.test(currency);
 
-export const isPositiveValue = (value: string): boolean =>
+export const isValidPositiveValue = (value: string): boolean =>
   !!value && currencyToNumber(value) > 0;
