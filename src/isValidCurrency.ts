085db2d3633eb575778c64120d5d264170a19819
Michael Amaral <michael.amaral@platformbuilders.io>
2020-12-15 14:33:48 -0300

    adding isPositiveValue

--- src/isValidCurrency.ts
+++ src/isValidCurrency.ts
@@ -1,2 +1,7 @@
+import { currencyToNumber } from '@platformbuilders/helpers';
+
 export const isValidCurrency = (currency: string): boolean =>
   !!currency && /^R\$[1-9]\d{0,2}(\.\d{3})*,\d{2}$/.test(currency);
+
+export const isPositiveValue = (value: string): boolean =>
+  !!value && currencyToNumber(value) > 0;
