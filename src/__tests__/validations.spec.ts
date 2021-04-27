24e094bf6663588215c38e4bd4e8a7c3176ac1b3
Glauber Brack <glauber@brack.com.br>
2021-04-27 11:37:51 -0300

    feat: add tests

--- src/__tests__/validations.spec.ts
+++ src/__tests__/validations.spec.ts
@@ -5,6 +5,7 @@ import { isValidEmail } from '../isValidEmail';
 import { isValidFullName } from '../isValidFullName';
 import { isValidCpf } from '../isValidCpf';
 import { isValidAccountBank } from '../isValidAccountBank';
+import { isValidAlphanumeric } from '../isValidAlphanumeric';
 
 jest.mock('../index', () => ({
   toOnlyNumbers: jest.fn((value) => value),
@@ -230,6 +231,40 @@ describe('Helper: formValidators', () => {
 
       // then
       expect(result).toBe(false);
+    });
+  });
+
+  describe('isValidAlphanumeric', () => {
+    test('when has an special character, then return false', () => {
+      // when
+      const result = isValidAlphanumeric('Glauber@');
+
+      // then
+      expect(result).toBe(false);
+    });
+
+    test('when has only letters, the return true', () => {
+      // when
+      const result = isValidAlphanumeric('Glauber');
+
+      // then
+      expect(result).toBe(true);
+    });
+
+    test('when has only numbers, the return true', () => {
+      // when
+      const result = isValidAlphanumeric('12345');
+
+      // then
+      expect(result).toBe(true);
+    });
+
+    test('when has mixed letters and numbers, the return true', () => {
+      // when
+      const result = isValidAlphanumeric('Glauber2021');
+
+      // then
+      expect(result).toBe(true);
     });
   });
 });
