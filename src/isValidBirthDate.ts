4a579266a35f35d47082825a56c092b0921de230
Fabiano <fabinppk@hotmail.com>
2022-11-23 22:25:27 -0300

    chore: deps update

--- src/isValidBirthDate.ts
+++ src/isValidBirthDate.ts
@@ -1,39 +1,39 @@
-import DateManager from 'moment';
+import moment from 'moment';
 
 const SCREEN_DATE = 'DD/MM/YYYY';
 
 export const isValidBirthDate = (date?: string): boolean => {
   const maxAge = 95;
   const minAge = 1;
-  const minDate = DateManager().subtract(maxAge, 'years');
-  const maxDate = DateManager().subtract(minAge, 'years');
-  const insertedDate = DateManager(date, SCREEN_DATE);
+  const minDate = moment().subtract(maxAge, 'years');
+  const maxDate = moment().subtract(minAge, 'years');
+  const insertedDate = moment(date, SCREEN_DATE);
   return insertedDate.isBetween(minDate, maxDate);
 };
 
 export const isValidLegalAge = (date?: string): boolean => {
   const maxAge = 95;
   const minAge = 18;
-  const minDate = DateManager().subtract(maxAge, 'years');
-  const maxDate = DateManager().subtract(minAge, 'years');
-  const insertedDate = DateManager(date, SCREEN_DATE);
+  const minDate = moment().subtract(maxAge, 'years');
+  const maxDate = moment().subtract(minAge, 'years');
+  const insertedDate = moment(date, SCREEN_DATE);
   return insertedDate.isBetween(minDate, maxDate);
 };
 
 export const isValidMinimumAge = (date?: string, minimumAge = 18): boolean => {
   const maxAge = 95;
   const minAge = minimumAge;
-  const minDate = DateManager().subtract(maxAge, 'years');
-  const maxDate = DateManager().subtract(minAge, 'years');
-  const insertedDate = DateManager(date, SCREEN_DATE);
+  const minDate = moment().subtract(maxAge, 'years');
+  const maxDate = moment().subtract(minAge, 'years');
+  const insertedDate = moment(date, SCREEN_DATE);
   return insertedDate.isBetween(minDate, maxDate);
 };
 
 export const isValidChildrenAge = (date?: string, minimumAge = 5): boolean => {
   const maxAge = 95;
-  const minDate = DateManager().subtract(maxAge, 'years');
-  const maxDate = DateManager().subtract(minimumAge, 'years');
-  const insertedDate = DateManager(date, SCREEN_DATE, true);
+  const minDate = moment().subtract(maxAge, 'years');
+  const maxDate = moment().subtract(minimumAge, 'years');
+  const insertedDate = moment(date, SCREEN_DATE, true);
 
   return insertedDate.isBetween(minDate, maxDate) && insertedDate.isValid();
 };
